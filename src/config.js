ce4f32922a5cca60406294a32a7d99845d063950
George Brown <sumthnabtgeorge@gmail.com>
2020-09-29 13:15:13 -0400

    small fixes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: "https://transactionary-api.herokuapp.com",
+  // API_ENDPOINT: "http://localhost:8000",
+  API_ENDPOINT: "https://transactionary-client-35ta71cgj.vercel.app/",
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
 };
